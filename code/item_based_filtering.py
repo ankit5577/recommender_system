5983c2cbf5e6373726bd0df90f5de4c329c6160e
Ankit Kaushal <33631555+ankit5577@users.noreply.github.com>
2021-09-10 22:27:36 +0530

    Update item_based_filtering.py

--- code/item_based_filtering.py
+++ code/item_based_filtering.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 
+# import data and rename (rows, cols)
 r_cols = ['user_id', 'movie_id', 'rating']
 ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols, usecols=range(3), encoding="ISO-8859-1")
-
 m_cols = ['movie_id', 'title']
 movies = pd.read_csv('ml-100k/u.item', sep='|', names=m_cols, usecols=range(2), encoding="ISO-8859-1")
 
+# merge
 ratings = pd.merge(movies, ratings)
 
 userRatings = ratings.pivot_table(index=['user_id'],columns=['title'],values='rating')
 
+# co-relation matrix
 corrMatrix = userRatings.corr(method='pearson', min_periods=100)
 
 myRatings = userRatings.loc[0].dropna()
